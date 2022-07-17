cdea1e34426e7c4baf5fce7f35fa9e369f513ec7
aiswaryanm03 <aishwaryanm03@gmail.com>
2022-07-17 16:49:17 +0530

    new class purchaseorder is created and added one more method in runTest class

--- src/test/java/GitTest/runTest.java
+++ src/test/java/GitTest/runTest.java
@@ -9,4 +9,9 @@ public class runTest {
 		System.out.println(" check for new test");
 		
 	}
+@Test
+public void runner1Test()
+{
+	System.out.println("new method runner2 is added");
+}
 }
---
+++ src/test/java/PurchaseorderTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class PurchaseorderTest {
+	@Test
+	public void createpurchaseorder()
+	{
+		System.out.println("new purchase order is created");
+	}
+
+}
