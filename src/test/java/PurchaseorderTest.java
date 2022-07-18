d5820340bd021c122a57445409d2193ca1326a1c
srigowri98 <gowri6498@gmail.com>
2022-07-18 11:50:13 +0530

    dteb

--- src/test/java/PurchaseorderTest.java
+++ src/test/java/PurchaseorderTest.java
@@ -10,13 +10,15 @@ public class PurchaseorderTest {
 		int a= 10;
 		System.out.println(a);
 		System.out.println(" new thing is encountered.....");
-		
+		System.out.println("new print statement");
 	}
 	@Test
 	public void Changesinpurchaseorder()
 	{
-		System.out.println("chnge in purchaseorder.....");
+		System.out.println("chnage in purchaseorder.....");
+		
 	}
+	
 	
 
 }
